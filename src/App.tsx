2026bc018a75d59c3ef46e424603d4c3c775f4e0
giacomotolari <tolarigiacomo@gmail.com>
2023-12-06 15:23:36 +0100

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { useState } from "react";
-import CounterBtn from "./components/CounterBtn";
-
-
-// Pass the setCount function as a prop to the CounterBtn component.
-// In the CounterBtn component, use the setCount function to update the count state.
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+
+  const handleNumbers = () => {
+    setNumbers((prevNumbers) => [...prevNumbers, 6])
+  }
+
   return (
     <>
-      <p>count is {count}</p>
-      <CounterBtn setCount={setCount}/>
+      <p>count is {numbers}</p>
+      <button onClick={handleNumbers}>update numbers</button>
     </>
   );
 }
